4f59998a52d7d7ba54f36e1041c9b8012e9b89c9
Rachel Givens <rachelgivens@me.com>
2019-05-18 21:31:46 -0500

    Last commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,27 +21,28 @@ $(document).ready(function () {
 
     var win = 0;
 
-    var loose = 0;
+    var loss = 0;
 
-    var numberOption = [20, 6, 5, 12];
+    var colorOption = ["clear", "pink", "purple", "blue"]
 
-    for (var i = 0; i < numberOption.length; i++) {
+    function buildCrystal(color){
+        var randomNumber = Math.floor(Math.random() * 10);
+        console.log(color, randomNumber);
 
         var imageCrystal = $("<img>");
 
         imageCrystal.addClass("crystal-image");
 
-        imageCrystal.attr("src", "./assets/images/clear crystal.jpg");
+        imageCrystal.attr("src", "./assets/images/" + color + "-crystal.jpg");
 
-        imageCrystal.attr("src", "./assets/images/pink crystal.jpg");
-
-        imageCrystal.attr("src", "./assets/images/purple crystal.jpeg");
-
-        imageCrystal.attr("src", "./assets/images/blue crystal.jpg");
-
-        imageCrystal.attr("data-crystalvalue", numberOption[i]);
+        imageCrystal.attr("data-crystalvalue", randomNumber);
 
         $("#crystals").append(imageCrystal);
+    }
+
+    for (var i = 0; i < colorOption.length; i++) {
+        buildCrystal(colorOption[i])
+       
     }
 
     $(".crystal-image").on("click", function () {
@@ -62,6 +63,8 @@ $(document).ready(function () {
         if (counter === tarNum) {
             alert("You won!");
             win++;
+            $("#crystals").append(win);
+            console.log(win);
             counter = 0;
             alert(counter);
             remainder = 0;
@@ -70,9 +73,11 @@ $(document).ready(function () {
 
         //Then output the number of times crystal is clicked 
 
-        else if (counter >= tarNum) {
+        else if (counter > tarNum) {
             alert("You loose!!");
             loss++;
+            $("#crystals").append(loss);
+            console.log(loss);
             counter = 0;
             alert(counter);
             remainder = 0;
